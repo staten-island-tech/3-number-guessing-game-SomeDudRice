2dd2badb51084d1f8615185776f751dea18a5f65
SomeDudRice <Lucasl197@nycstudents.net>
2026-02-26 09:37:47 -0500

    Update idledeathgamble.py

--- idledeathgamble.py
+++ idledeathgamble.py
@@ -1,4 +1,11 @@
-x=10
-while x > 0:
- print (x)
- x = x - 1
+# x=10
+# while x > 0:
+#  print (x)
+#  x = x - 1
+
+color = ""
+
+while color != "stop":
+    color = input("What is your favorite color? (type 'stop' to finish): ")
+
+print("glad to know your favorite color is (color)!")
