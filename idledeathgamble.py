51e24b47d4565799be15deb9276c8e576d44f0a6
SomeDudRice <Lucasl197@nycstudents.net>
2026-03-16 10:14:58 -0400

    Update idledeathgamble.py

--- idledeathgamble.py
+++ idledeathgamble.py
@@ -10,18 +10,36 @@
 # print("glad to know your favorite color !")
 
 
-# righty_thighty = 0
-# import random
-# my_list = []
-# answer = random.randint(1,10)
-# while righty_thighty == 0:
-#  isput = int(input("number"))
-#  if isput == answer:
-#   righty_thighty ==  righty_thighty + 1
-#  elif isput > righty_thighty:
-#   print ("too high")
-#  elif isput < righty_thighty:
-#   print ('too low')
+righty_tighty = 0
+import random
+my_list = []
+answer = random.randint(1,10)
+isput = int(input("number"))
+guessnum =0
+while not righty_tighty == 1:
+ if isput == answer:
+  my_list.insert(guessnum, isput)
+  righty_tighty ==  righty_tighty + 1
+  break
+ elif isput > answer:
+  guessnum =+ 1
+  my_list.insert(guessnum, isput)
+  print ("too high")
+  isput = int(input("number"))
+ elif isput < answer:
+  print ('too low')
+  my_list.insert(guessnum, isput)
+  guessnum =+ 1
+  isput = int(input("number"))
+ else:
+  print("invalid")
+  guessnum =+ 1
+  my_list.insert(guessnum, isput)
+  isput = int(input("number"))
+
+
+
+print(isput, "is correct, here is your history",my_list)
 
 
 # quart = 77
@@ -56,21 +74,21 @@
 #    m3 =0
 # print(plays)
 
-def casino (quart,spins,pyaouts,jackpots, ):
-    mturn=0
-    plays= 0
-    while quart > 0:
-     if jackpots[mturn] > spins[mturn]:
-      plays+=1
-      quart-=1
-      spins[mturn]+=1
-      if jackpots[mturn] == spins[mturn]:
-        quart+=pyaouts[mturn]
-     mturn+=1
-     if mturn ==2:
-      mturn ==0
+# def casino (quart,spins,pyaouts,jackpots, ):
+#     mturn=0
+#     plays= 0
+#     while quart > 0:
+#      if jackpots[mturn] > spins[mturn]:
+#       plays+=1
+#       quart-=1
+#       spins[mturn]+=1
+#       if jackpots[mturn] == spins[mturn]:
+#         quart+=pyaouts[mturn]
+#      mturn+=1
+#      if mturn ==2:
+#       mturn ==0
 
-casino(77,[4,9,3],[30,60,9],[60, 100,10])
+# casino(77,[4,9,3],[30,60,9],[60, 100,10])
 
 # print ('wrong')
 # my_list.insert(isput)
